b4ae90d71f22ad6700bfac73b47cee47e4c7e6b4
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2020-11-19 16:22:18 +0000

    Revert "Maps NALD charge versions to WRLS format"

--- test/modules/charging-import/lib/import.js
+++ test/modules/charging-import/lib/import.js
@@ -9,14 +9,14 @@ const chargingQueries = require('../../../../src/modules/charging-import/lib/que
 const returnVersionQueries = require('../../../../src/modules/charging-import/lib/queries/return-versions');
 const financialAgreementTypeQueries = require('../../../../src/modules/charging-import/lib/queries/financial-agreement-types');
 const purposesQueries = require('../../../../src/modules/charging-import/lib/queries/purposes');
-const chargeVersionImportService = require('../../../../src/modules/charging-import/services/charge-version-import');
+const checkIntegrity = require('../../../../src/modules/charging-import/lib/check-integrity');
 
 experiment('modules/charging-import/index.js', () => {
   beforeEach(async () => {
     sandbox.stub(logger, 'info');
     sandbox.stub(logger, 'error');
     sandbox.stub(pool, 'query');
-    sandbox.stub(chargeVersionImportService, 'importChargeVersions');
+    sandbox.stub(checkIntegrity, 'verify');
   });
 
   afterEach(async () => {
@@ -36,17 +36,16 @@ experiment('modules/charging-import/index.js', () => {
       });
     });
 
-    experiment('when there are no errors', () => {
+    experiment('when there are no verification errors', () => {
       beforeEach(async () => {
+        checkIntegrity.verify.resolves({
+          totalErrors: 0
+        });
         await chargingImport.importChargingData();
       });
 
-      test('runs the charge version import process', async () => {
-        expect(chargeVersionImportService.importChargeVersions.called).to.be.true();
-      });
-
       test('logs info messages', async () => {
-        expect(logger.info.callCount).to.equal(2);
+        expect(logger.info.callCount).to.equal(3);
       });
 
       test('does not log error messages', async () => {
@@ -59,11 +58,26 @@ experiment('modules/charging-import/index.js', () => {
         expect(pool.query.getCall(2).args[0]).to.equal(purposesQueries.importSecondaryPurposes);
         expect(pool.query.getCall(3).args[0]).to.equal(purposesQueries.importUses);
         expect(pool.query.getCall(4).args[0]).to.equal(purposesQueries.importValidPurposeCombinations);
-        expect(pool.query.getCall(5).args[0]).to.equal(chargingQueries.importChargeElements);
-        expect(pool.query.getCall(6).args[0]).to.equal(chargingQueries.cleanupChargeElements);
-        expect(pool.query.getCall(7).args[0]).to.equal(returnVersionQueries.importReturnVersions);
-        expect(pool.query.getCall(8).args[0]).to.equal(returnVersionQueries.importReturnRequirements);
-        expect(pool.query.getCall(9).args[0]).to.equal(returnVersionQueries.importReturnRequirementPurposes);
+        expect(pool.query.getCall(5).args[0]).to.equal(chargingQueries.importChargeVersions);
+        expect(pool.query.getCall(6).args[0]).to.equal(chargingQueries.importChargeElements);
+        expect(pool.query.getCall(7).args[0]).to.equal(chargingQueries.cleanupChargeElements);
+        expect(pool.query.getCall(8).args[0]).to.equal(chargingQueries.cleanupChargeVersions);
+        expect(pool.query.getCall(9).args[0]).to.equal(chargingQueries.updateChargeVersionsLicenceId);
+        expect(pool.query.getCall(10).args[0]).to.equal(returnVersionQueries.importReturnVersions);
+        expect(pool.query.getCall(11).args[0]).to.equal(returnVersionQueries.importReturnRequirements);
+        expect(pool.query.getCall(12).args[0]).to.equal(returnVersionQueries.importReturnRequirementPurposes);
+      });
+    });
+    experiment('when there are verification errors', () => {
+      beforeEach(async () => {
+        checkIntegrity.verify.resolves({
+          totalErrors: 1
+        });
+        await chargingImport.importChargingData();
+      });
+
+      test('logs error message', async () => {
+        expect(logger.error.callCount).to.equal(1);
       });
     });
   });
